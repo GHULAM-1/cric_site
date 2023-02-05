f10eb15cdd1b7f34694720f10a9970c78fc7bdea
GHULAM-1 <ilyasghulam35@gmail.com>
2023-02-05 13:13:34 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Home } from './pages/Home'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Home />
+
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return <React.Fragment>
+    <h1>ajwad </h1>
+  </React.Fragment>
+};
+
+export {Home};
